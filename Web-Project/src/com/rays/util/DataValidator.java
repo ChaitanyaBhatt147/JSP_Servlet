647c50ffec676025239ee059f00b9bd00f3149be
ChaitanyaBhatt147 <bhattchaitanya43@gmail.com>
2025-10-15 22:58:03 +0530

    done

---
+++ Web-Project/src/com/rays/util/DataValidator.java
@@ -0,0 +1,13 @@
+package com.rays.util;
+
+import javax.servlet.http.HttpServlet;
+
+public class DataValidator {
+	public static boolean signupValitation(HttpServlet request) {
+		boolean isValid = true;
+		
+		
+		return isValid;
+	}
+
+}
